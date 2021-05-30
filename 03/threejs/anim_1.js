1c000a051effeb6cd906ef40d8462ee2a2de2760
Leonardo Ventura <leonardo.venturacl@gmail.com>
2021-05-30 17:50:36 -0300

    Add waiving and robot implementation

--- 03/threejs/anim_1.js
+++ 03/threejs/anim_1.js
@@ -4,27 +4,107 @@ Object.assign( WaveAnimation.prototype, {
 
     init: function() {
         let upperArmTween = new TWEEN.Tween( {theta:0} )
-            .to( {theta:Math.PI }, 500)
+            .to( {theta:Math.PI/2 }, 500)
             .onUpdate(function(){
                 // This is an example of rotation of the right_upper_arm 
                 // Notice that the transform is M = T * R 
-                let right_upper_arm =  robot.getObjectByName("right_upper_arm");
-                right_upper_arm.matrix.makeRotationZ(this._object.theta).premultiply( new THREE.Matrix4().makeTranslation(2.6, 0, 0 ) );
-
+                let right_upper_arm = robot.getObjectByName("right_upper_arm");
+                const pivot = {x: 0, y: 2, z:0};
+                const {x, y, z} = right_upper_arm.position;
+                right_upper_arm.matrix.makeTranslation(0,0,0)
+                    .premultiply( new THREE.Matrix4().makeTranslation(-pivot.x, -pivot.y, -pivot.z))
+                    .premultiply( new THREE.Matrix4().makeRotationZ(this._object.theta))
+                    .premultiply( new THREE.Matrix4().makeTranslation(pivot.x, pivot.y, pivot.z))
+                    .premultiply( new THREE.Matrix4().makeTranslation(x, y, z));
 
 
                 // Updating final world matrix (with parent transforms) - mandatory
                 right_upper_arm.updateMatrixWorld(true);
                 // Updating screen
                 stats.update();
-                renderer.render(scene, camera);    
+                renderer.render(scene, camera);
             })
-        // Here you may include animations for other parts 
-            
-    
-        
-        //  upperArmTween.chain( ... ); this allows other related Tween animations occur at the same time
-        upperArmTween.start();       
+
+        let lowerArmTween = new TWEEN.Tween( {theta:0} )
+            .to( {theta:Math.PI/2 }, 500)
+            .onUpdate(function(){
+                let right_lower_arm = robot.getObjectByName("right_upper_arm")
+                    .getObjectByName("lower_arm");
+
+                const pivot = {x: 0, y: 1.5, z:0};
+                const {x, y, z} = right_lower_arm.position;
+                right_lower_arm.matrix.makeTranslation(0,0,0)
+                    .premultiply( new THREE.Matrix4().makeTranslation(-pivot.x, -pivot.y, -pivot.z))
+                    .premultiply( new THREE.Matrix4().makeRotationZ(this._object.theta))
+                    .premultiply( new THREE.Matrix4().makeTranslation(pivot.x, pivot.y, pivot.z))
+                    .premultiply( new THREE.Matrix4().makeTranslation(x, y, z));
+
+                right_lower_arm.updateMatrixWorld(true);
+                stats.update();
+                renderer.render(scene, camera);
+            })
+
+        let handTween = new TWEEN.Tween( {theta:0} )
+            .to( {theta:Math.PI/30 }, 300)
+            .onUpdate(function(){
+                let hand = robot.getObjectByName("right_upper_arm")
+                    .getObjectByName("lower_arm").getObjectByName("hand");
+
+                const {x, y, z} = hand.position;
+                hand.matrix.makeTranslation(0,0,0)
+                    .premultiply( new THREE.Matrix4().makeRotationZ(this._object.theta))
+                    .premultiply( new THREE.Matrix4().makeTranslation(x, y, z));
+
+                hand.updateMatrixWorld(true);
+                stats.update();
+                renderer.render(scene, camera);
+            }).repeat(5).yoyo(true);
+
+        let lowerArmTweenReturn = new TWEEN.Tween( {theta:Math.PI/2} )
+            .to( {theta:0 }, 500)
+            .onUpdate(function(){
+                let right_lower_arm = robot.getObjectByName("right_upper_arm")
+                    .getObjectByName("lower_arm");
+
+                const pivot = {x: 0, y: 1.5, z:0};
+                const {x, y, z} = right_lower_arm.position;
+                right_lower_arm.matrix.makeTranslation(0,0,0)
+                    .premultiply( new THREE.Matrix4().makeTranslation(-pivot.x, -pivot.y, -pivot.z))
+                    .premultiply( new THREE.Matrix4().makeRotationZ(this._object.theta))
+                    .premultiply( new THREE.Matrix4().makeTranslation(pivot.x, pivot.y, pivot.z))
+                    .premultiply( new THREE.Matrix4().makeTranslation(x, y, z));
+
+                right_lower_arm.updateMatrixWorld(true);
+                stats.update();
+                renderer.render(scene, camera);
+            })
+
+        let upperArmTweenReturn = new TWEEN.Tween( {theta:Math.PI/2} )
+            .to( {theta:0 }, 500)
+            .onUpdate(function(){
+                // This is an example of rotation of the right_upper_arm 
+                // Notice that the transform is M = T * R 
+                let right_upper_arm = robot.getObjectByName("right_upper_arm");
+                const pivot = {x: 0, y: 2, z:0};
+                const {x, y, z} = right_upper_arm.position;
+                right_upper_arm.matrix.makeTranslation(0,0,0)
+                    .premultiply( new THREE.Matrix4().makeTranslation(-pivot.x, -pivot.y, -pivot.z))
+                    .premultiply( new THREE.Matrix4().makeRotationZ(this._object.theta))
+                    .premultiply( new THREE.Matrix4().makeTranslation(pivot.x, pivot.y, pivot.z))
+                    .premultiply( new THREE.Matrix4().makeTranslation(x, y, z));
+
+
+                // Updating final world matrix (with parent transforms) - mandatory
+                right_upper_arm.updateMatrixWorld(true);
+                // Updating screen
+                stats.update();
+                renderer.render(scene, camera);
+            })
+
+        upperArmTween.start();
+        lowerArmTween.start();
+        lowerArmTween.chain(handTween);
+        handTween.chain(lowerArmTweenReturn, upperArmTweenReturn);
     },
     animate: function(time) {
         window.requestAnimationFrame(this.animate.bind(this));
--- 03/threejs/robot.js
+++ 03/threejs/robot.js
@@ -29,24 +29,36 @@ function gen_robot() {
     left_upper_arm.position.x = -2.6;
 
     // right: upper arm, arm, hand
-    var right_upper_arm = left_upper_arm.clone();  
+    var right_upper_arm = left_upper_arm.clone();
     right_upper_arm.name = "right_upper_arm";
     right_upper_arm.position.x = 2.6;
-    
 
     // left: upper leg, leg, foot
-    // TO DO
+    var left_upper_leg = gen_rect(1.5, 4);
+    left_upper_leg.name = "left_upper_leg";
+    var left_lower_leg = gen_rect(1, 3);
+    left_lower_leg.name = "lower_leg";
+    var left_foot = gen_rect(1.5, 0.5);
+    left_foot.name = "foot";
+    left_upper_leg.add(left_lower_leg);
+    left_lower_leg.add(left_foot);
+    left_upper_leg.position.y = -4;
+    left_foot.position.y = -1.5;
+    left_lower_leg.position.y = -3;
+    left_upper_leg.position.x = -1;
 
     // right: upper leg, leg, foot
-    // TO DO
+    var right_upper_leg = left_upper_leg.clone();
+    right_upper_leg.name = "right_upper_leg";
+    right_upper_leg.position.x = 1;
 
     // Creating hieararchy
     robot.add(torso);
     torso.add(right_upper_arm);
     torso.add(head);
     torso.add(left_upper_arm);
-    // TO DO: add remaining robot parts hierarchical relations
-
+    torso.add(left_upper_leg);
+    torso.add(right_upper_leg);
 
     robot.name = "robot";
 
