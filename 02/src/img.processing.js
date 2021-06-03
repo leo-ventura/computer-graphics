0ae22f8f9325e03ca630dfbe01d8577d3211f363
Leonardo Ventura <leonardo.venturacl@gmail.com>
2021-06-03 15:40:48 -0300

    Add first version of kernels

--- 02/src/img.processing.js
+++ 02/src/img.processing.js
@@ -21,7 +21,101 @@
             // Method to apply kernel over image (incomplete)
             // border: 'icrop' is for cropping image borders, 'extend' is for extending image border
             // You may create auxiliary functions/methods if you'd like
+            const box = () => {
+                // Media aritmetica dos pixels cobertos pelo filtro
+                let filtered_img = this.img.clone();
 
+                for (let i = 1; i < this.width - 1; i++) {
+                    for (let j = 1; j < this.height - 1; j++) {
+                        const total = this.img.get(i-1, j-1)
+                                    + this.img.get(i  , j-1)
+                                    + this.img.get(i+1, j-1)
+                                    + this.img.get(i-1, j  )
+                                    + this.img.get(i  , j  )
+                                    + this.img.get(i+1, j  )
+                                    + this.img.get(i-1, j+1)
+                                    + this.img.get(i  , j+1)
+                                    + this.img.get(i+1, j+1)
+
+                        const pixel = total/9;
+
+                        filtered_img.set(i, j, pixel);
+                    }
+                }
+
+                return filtered_img;
+            };
+
+            const sobel = () => {
+                // Modulo do gradiente de I
+                // [ref]: https://pt.wikipedia.org/wiki/Filtro_Sobel
+                let filtered_img = this.img.clone();
+
+                for (let i = 1; i < this.width - 1; i++) {
+                    for (let j = 1; j < this.height - 1; j++) {
+                        const totalSx = this.img.get(i-1, j-1) * -1
+                                      + this.img.get(i  , j-1) * -2
+                                      + this.img.get(i+1, j-1) * -1
+                                      // + this.img.get(i-1, j  ) * 0
+                                      // + this.img.get(i  , j  ) * 0
+                                      // + this.img.get(i+1, j  ) * 0
+                                      + this.img.get(i-1, j+1) * 1
+                                      + this.img.get(i  , j+1) * 2
+                                      + this.img.get(i+1, j+1) * 1
+                        const X_ = Math.pow(totalSx/8, 2);
+
+                        const totalSy = this.img.get(i-1, j-1) * 1
+                                    //   + this.img.get(i  , j-1) * 0
+                                      + this.img.get(i+1, j-1) * -1
+                                      + this.img.get(i-1, j  ) * 2
+                                    //   + this.img.get(i  , j  ) * 0
+                                      + this.img.get(i+1, j  ) * -2
+                                      + this.img.get(i-1, j+1) * 1
+                                    //   + this.img.get(i  , j+1) * 0
+                                      + this.img.get(i+1, j+1) * -1
+
+                        const Y_ = Math.pow(totalSy/8, 2);
+
+                        const pixel = Math.sqrt(X_ + Y_);
+
+                        filtered_img.set(i, j, pixel);
+                    }
+                }
+
+                return filtered_img;
+            };
+
+            const laplace = () => {
+                let filtered_img = this.img.clone();
+
+                for (let i = 1; i < this.width - 1; i++) {
+                    for (let j = 1; j < this.height - 1; j++) {
+                        const total = this.img.get(i-1, j-1) * 0
+                                    + this.img.get(i  , j-1) * -1
+                                    // + this.img.get(i+1, j-1) * 0
+                                    + this.img.get(i-1, j  ) * -1
+                                    + this.img.get(i  , j  ) * 4
+                                    + this.img.get(i+1, j  ) * -1
+                                    // + this.img.get(i-1, j+1) * 0
+                                    + this.img.get(i  , j+1) * -1
+                                    // + this.img.get(i+1, j+1) * 0
+
+                        const pixel = total/4;
+
+                        filtered_img.set(i, j, pixel);
+                    }
+                }
+
+                return filtered_img;
+            };
+
+            const kernels = {
+                "box": box,
+                "sobel": sobel,
+                "laplace": laplace,
+            };
+
+            this.img = kernels[this.kernel]();
         },
 
         apply_xform: function()  {
@@ -54,7 +148,7 @@
 
 
     exports.ImageProcesser = ImageProcesser;
-    
-    
+
+
 })));
 
